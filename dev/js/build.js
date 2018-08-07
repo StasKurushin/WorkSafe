7ad211d0048859e0c5d1084f2556359883033fec
Stas Kurushin <kurushinstas@gmail.com>
2018-08-07 17:27:06 +0300

    -//-

--- dev/js/build.js
+++ dev/js/build.js
@@ -17,14 +17,10 @@ const navbarListItem = document.querySelectorAll('.navbar-list__item');
 (function () {
     let navbar = document.querySelector('.navbar-list');
     let headerTitle = document.querySelector('.header__title');
-    let startTitle = document.querySelector('.start__title');
-    let start = document.querySelector('.start');
-    let target = document.querySelector('.start-container');
-    let tId;
+    let intro = document.querySelector('.header__intro');
 
     window.onload = function () {
         if (window.scrollY < 50) {
-            startAnim();
             headerAnim();
         } else {
             if (document.documentElement.clientWidth > 767) {
@@ -39,43 +35,25 @@ const navbarListItem = document.querySelectorAll('.navbar-list__item');
         }
     };
 
-    function shine() {
-        let op = 0;
-        tId = setTimeout(function tick() {
-            op += 0.025;
-            startTitle.style.opacity = op;
-            if (op < 0.7) {
-                tId = setTimeout(tick, 100)
-            }
-        });
-    }
-    function rotate() {
-        target.classList.add('start-container--rotate');
-    }
-    function startAnim() {
-        start.style.display = 'block';
-        shine();
-        setTimeout(rotate, 2500);
-
-    }
     function headerAnim() {
         setTimeout( function () {
-            start.style.display = 'none';
-            clearTimeout(tId);
             if (document.documentElement.clientWidth > 767) {
                 navbar.classList.add('navbar-list--slide');
                 headerTitle.classList.add('header__title--slide');
+                intro.classList.add('header__intro--scaleup');
                 setTimeout(function () {
                     navbar.style.position = 'static';
                     headerTitle.style.position = 'static';
+
                 }, 700)
             } else {
                 navbar.style.transform = 'scale(1)';
                 headerTitle.style.transform = 'scale(1)';
                 navbar.classList.add('navbar-list--slide__min-screen');
                 headerTitle.classList.add('header__title--slide__min-screen');
+                intro.classList.add('header__intro--scaleup');
             }
-        }, 3000);
+        }, 500);
     }
 })();
 
